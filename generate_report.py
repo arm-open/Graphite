4576ce1f74b8d90f864ae04d1cd77e3382edf537
Arian <amoslem678@gmail.com>
2017-05-16 17:48:10 -0400

    Made the variable names more professional and commented out all the test echo's

--- generate_report.py
+++ generate_report.py
@@ -7,22 +7,22 @@ userpassword = ''
 @click.argument('pdfname')
 @click.option('--email', help='Google Analytics Email')
 @click.option('--password', help='Google Analytics Password')
-def setStuff(pdfname, email, password):
+def importArgs(pdfname, email, password):
     global useremail 
     useremail = email
     global userpassword
     userpassword = password
     click.echo(pdfname)
-    printEmail()
-    printPwd()
+    #printEmail()
+    #printPwd()
 
+''''
 def printEmail():
     click.echo('%s' % useremail)
 
 def printPwd():
     click.echo('%s' % userpassword)
+'''
 
 if __name__ == '__main__':
-    setStuff()
-    printEmail()
-    printPwd()
+    importArgs()
