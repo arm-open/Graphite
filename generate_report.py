f2a4650bc993c8938e7c3c93d54c3f2fac1511ba
Arian <amoslem678@gmail.com>
2017-05-17 16:58:54 -0400

    Fixed arguments for API Key

--- generate_report.py
+++ generate_report.py
@@ -1,28 +1,24 @@
+#Import the rest here later
 import click
+from apiclient.discovery import build
 
-useremail = ''
-userpassword = ''
 
+#Global Variables to account for email and password
+pdfToOpen = ''
+aKey = ''
+#API Service
+
+#Instantiate a click command
 @click.command()
 @click.argument('pdfname')
-@click.option('--email', help='Google Analytics Email')
-@click.option('--password', help='Google Analytics Password')
-def importArgs(pdfname, email, password):
-    global useremail 
-    useremail = email
-    global userpassword
-    userpassword = password
-    click.echo(pdfname)
-    #printEmail()
-    #printPwd()
-
-''''
-def printEmail():
-    click.echo('%s' % useremail)
-
-def printPwd():
-    click.echo('%s' % userpassword)
-'''
+@click.argument('apikey')
+def importArgs(pdfname, apikey):
+    global pdfToOpen
+    pdfToOpen = pdfToOpen
+    global akey
+    aKey = apikey
+    analytics_service = build('analytics', 'v3', developerKey = aKey)
 
 if __name__ == '__main__':
     importArgs()
+
